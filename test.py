6c1d2a14f2dc097e560b1f252f34105f2fcaa9ba
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-06 21:15:08 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+fruits = ["apple","mango","orange"]
+vegetables = ["carrot","potato","onion"]
+meats = ["fish","chicken","beaf"]
+
+groceories = [fruits,vegetables,meats]
+
+print(groceories)
+ 
+'''
+[['apple', 'mango', 'orange'], ['carrot', 'potato', 'onion'], ['fish', 'chicken', 'beaf']]
+'''
+fruits [0] = "banana"
+print(groceories)
+#O/P -[['banana', 'mango', 'orange'], ['carrot', 'potato', 'onion'], ['fish', 'chicken', 'beaf']]
+
